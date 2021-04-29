8d09dfb46b8f72eeca38effd799840148cbe9fe8
JRsams <jack.sams@unai.com>
2021-04-29 15:23:24 +0100

    initial commit

--- stockManager/src/main/java/stockBPM/stockManager/CamundaApplication.java
+++ stockManager/src/main/java/stockBPM/stockManager/CamundaApplication.java
@@ -1,8 +1,8 @@
 package stockBPM.stockManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.ProcessEngines;
 import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +13,8 @@ public class CamundaApplication {
 
 	public static void main(String... args) throws SQLException {
 		SpringApplication.run(CamundaApplication.class, args);
+//		ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+//		processEngine.getRuntimeService().startProcessInstanceByKey("stockManager");
 	}
 
 }

e53927eead7c125ea70224798ffb388c20376d4e
JRsams <jack.sams@unai.com>
2021-04-29 11:51:14 +0100

    update class definitions

--- stockManager/src/main/java/stockBPM/stockManager/stockController.java
+++ stockManager/src/main/java/stockBPM/stockManager/stockController.java
@@ -3,7 +3,8 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,16 +12,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("stock")
 public class stockController {
 
-	private Database stockData = new Database("localhost:3306","stock","camundabpm","disp20", null);
+	private DatabaseController stockData = new DatabaseController("localhost:3306","stock","camundabpm","disp20", null);
 	
 	public stockController() {
 	stockData.openConnection();
 	}
 	
 	@GetMapping("list")
-	public ArrayList<Object> request() throws SQLException{
-		ArrayList<Object> data = (stockData.query("select * from fish"));
+	public ArrayList<Object> listStock() throws SQLException{
+		ArrayList<Object> data = (stockData.read("select * from fish"));
 	    return data;
+	}
+	
+	@PostMapping(value = "restock", consumes = "application/json")
+	public String restockItem (@RequestBody stockItem item) throws SQLException {
+		return stockData.restock(item.getName(), item.getQuantity()); 
+	}
+	
+	@PostMapping(value = "order", consumes = "application/json")
+	public Object newOrder (@RequestBody orderItem order) throws SQLException {
+		return stockData.newOrder(order); 
 	}
 
 }
